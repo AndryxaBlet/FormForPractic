8e1d6a92269a64bbed2baf6b436a4abdaf7a0df1
AndryxaBlet <x555xdb423@gmail.com>
2023-12-18 19:51:09 +0600

    1.0

---
+++ FormForPractic/GridWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace FormForPractic
+{
+    /// <summary>
+    /// Логика взаимодействия для GridWindow.xaml
+    /// </summary>
+    public partial class GridWindow : Window
+    {
+        public GridWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            AddForm form = new AddForm();
+            form.Owner = this;
+            form.ShowDialog();
+        }
+
+        private void minus_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Удалить запись под номером ... ?", "Подтверждение", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            if(result == MessageBoxResult.OK) {
+                MessageBox.Show("Успешно!", "Запись удалена", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        
+        }
+    }
+}
---
+++ FormForPractic/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace FormForPractic
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        { 
+            string connectionString= @"data source=REVISION-PC;initial catalog=practicir311;integrated security=True";
+          
+          
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                 SqlCommand command = new SqlCommand();
+                command.CommandText="select * from users where login = @log and password = @pass";
+                command.Connection = connection;
+                command.Parameters.AddWithValue("@pass", passb.Password.ToString());
+                command.Parameters.AddWithValue("@log", login.Text.ToString());
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+
+                        GridWindow frm = new GridWindow();
+                        frm.Show();
+                        Close();
+                    }
+                    else
+                    {
+                        var result = MessageBox.Show("Данные не верны!\nПопробовать ещё раз?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                        if (result == MessageBoxResult.No)
+                        {
+                            this.Close();
+                        }
+                    }
+                }
+            }
+
+          
+           
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
